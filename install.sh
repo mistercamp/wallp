b53db064f43935f41cddd570cfde3effd0e388b3
b3n <b3n@b3n.desktop>
2023-10-19 13:48:01 -0500

    cleaned up install

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 INSTALL_LOC='/opt/wallp'
 
-
 echo "SUDO required for install"
 sudo apt install python3-venv -y
 sudo mkdir -p $INSTALL_LOC/{tmp,img}
@@ -11,10 +10,7 @@ sudo python3 -m venv $INSTALL_LOC/.venv
 sudo $INSTALL_LOC/.venv/bin/python3 -m pip install -r $INSTALL_LOC/requirements.txt
 sudo chown -R $USER:$USER $INSTALL_LOC
 
-sed -i "s|__USER__|$USER|" wallp.service
-sed -i "s|__DISPLAY__|$DISPLAY|" wallp.service
-sed -i "s|__XAUTH__|$XAUTHORITY|" wallp.service
-sed -i "s|__XDGRUN__|$XDG_RUNTIME_DIR|" wallp.service
+sed -i "s|__USER__|$USER| ; s|__DISPLAY__|$DISPLAY| ; s|__XAUTH__|$XAUTHORITY| ; s|__XDGRUN__|$XDG_RUNTIME_DIR|" wallp.service
 
 if [ -f /etc/systemd/system/wallp.service ]; then
     sudo systemctl stop wallp.service
